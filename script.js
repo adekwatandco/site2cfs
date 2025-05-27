b1641d0ffd5654d27f9f94b95a6a4275c2fe77a6
adekwatandco <adekwatandco@gmail.com>
2025-05-27 20:54:37 +0200

    tuesday

--- script.js
+++ script.js
@@ -17,3 +17,26 @@
       modal.style.display = 'none';
     }
   });
+
+  
+  // let currentIndex = 0;
+
+  // function showSlide(index) {
+  //   const carousel = document.getElementById('carouselImages');
+  //   const totalSlides = carousel.children.length;
+  //   if (index >= totalSlides) currentIndex = 0;
+  //   else if (index < 0) currentIndex = totalSlides - 1;
+  //   else currentIndex = index;
+
+  //   const offset = -currentIndex * 100;
+  //   carousel.style.transform = `translateX(${offset}%)`;
+  // }
+
+  // function nextSlide() {
+  //   showSlide(currentIndex + 1);
+  // }
+
+  // function prevSlide() {
+  //   showSlide(currentIndex - 1);
+  // }
+
