62c9aba5b33718fb1cfab20c9340c7fdd0fcce2e
adekwatandco <adekwatandco@gmail.com>
2025-05-28 13:42:01 +0200

    lunch

--- script.js
+++ script.js
@@ -19,4 +19,17 @@
   });
 
   
-  
+  //fomulaire register//
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".form-subscribe");
+    const email = document.getElementById("email");
+    const emailConfirm = document.getElementById("email-confirm");
+
+    form.addEventListener("submit", function (e) {
+      if (email.value !== emailConfirm.value) {
+        e.preventDefault(); 
+        alert("adresse email non conformea");
+        emailConfirm.focus();
+      }
+    });
+  });
